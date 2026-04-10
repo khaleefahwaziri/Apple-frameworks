be1cb8571e9fa6553ab0e4169de634e5e01e4e0f
khaleefahwaziri <khaleefahabubakar39@gmail.com>
2026-04-10 18:21:54 +0100

    minor spellings correction

--- Apple-Frameworks/ContentView.swift
+++ Apple-Frameworks/ContentView.swift
@@ -6,45 +6,45 @@
 //
 
 import SwiftUI
-    
-let columns: [GridItem] = [GridItem(.flexible()),
-                           GridItem(.flexible()),
-                           GridItem(.flexible())                                    
-                          ]
 
 struct ContentView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
     var body: some View {
         LazyVGrid(columns: columns) {
             ForEach(MockData.frameworks) { framework in
-                FrameworkTitleView(framework: framework)
+                frameworkTitleView(framework: framework)
             }
+            
         }
-         
     }
 }
-
 
 #Preview {
     ContentView()
 }
 
 
-struct FrameworkTitleView: View {
+
+struct frameworkTitleView: View {
+    
     let framework: Framework
     
     var body: some View {
-        VStack {
+        VStack{
             Image(framework.imageName)
                 .resizable()
                 .frame(width: 90, height: 90)
+                
             Text(framework.name)
                 .font(.title2)
-                .fontWeight(.semibold)
+                .fontWeight(.bold)
                 .scaledToFit()
                 .minimumScaleFactor(0.6)
         }
-            
     }
 }
-
 
--- Apple-Frameworks/Framework.swift
+++ Apple-Frameworks/Framework.swift
@@ -7,12 +7,14 @@
 
 import Foundation
 
-struct Framework: Hashable, Identifiable  {
+struct Framework: Identifiable, Hashable {
+    
     let id = UUID()
     let name: String
     let imageName: String
     let urlString: String
     let description: String
+    
 }
 
 struct MockData {
